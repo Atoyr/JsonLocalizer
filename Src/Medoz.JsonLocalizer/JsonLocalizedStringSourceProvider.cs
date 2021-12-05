282a5640bbec1fc5382b3c002fbce7bbf5e43a89
Atoyr <lessironglance@gmail.com>
2021-12-05 17:16:48 +0900

    fix path is not working

--- Src/Medoz.JsonLocalizer/JsonLocalizedStringSourceProvider.cs
+++ Src/Medoz.JsonLocalizer/JsonLocalizedStringSourceProvider.cs
@@ -13,20 +13,24 @@ namespace Medoz.JsonLocalizer
     {
         private string _resourcePath { get; set; } = "Resources";
         private string _baseFileName { get; set; } = "languageResource";
+
+        private bool _useFullName { get; set; } = true;
         private const string extend = ".json";
 
         public JsonLocalizedStringSourceProvider() {}
-        public JsonLocalizedStringSourceProvider(string resourcePath, string baseFileName)
+        public JsonLocalizedStringSourceProvider(string resourcePath, string baseFileName, bool useFullName = true)
         {
             _resourcePath = resourcePath;
             _baseFileName = baseFileName;
+            _useFullName = useFullName;
         }
 
         public JsonLocalizedStringSource GetLocalizedStringSource(string baseName, string location)
         {
             IDictionary<string, IDictionary<string, string>> resource;
 
-            var targetFileName1 = Path.Combine(location, baseName) + extend;
+            var locationPath = location.Replace('.','\\');
+            var targetFileName1 = _useFullName ? Path.Combine(locationPath, baseName) + extend : baseName + extend;
             var targetFileName2 = targetFileName1.Replace('\\','.');
 
             if (File.Exists(targetFileName1))
